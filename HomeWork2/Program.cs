7d3bf31143a5211af620606bbb1f41d13e6f4d56
Dmitry Nagovitsyn <nagovicinde@mail.ru>
2022-11-18 22:20:22 +0400

    HomeWork2 added

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 2: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+int Prompt(string message)
+{
+    System.Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int[,] CreateArray(int rous, int colums)
+{
+    int[,] massiv = new int[rous, colums];
+    Random rnd = new Random();
+    for (int i = 0; i < massiv.GetLength(0); i++)
+    {
+        for (int j = 0; j < massiv.GetLength(1); j++)
+        {
+            massiv[i, j] = rnd.Next(0, 11);
+        }
+    }
+    return massiv;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void SumArrows(int[,] array)
+{
+    int index = 0, minsum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        Console.WriteLine($"Сумма {i} строки = {sum}");
+        if (i == 0)
+        {
+            minsum = sum;
+        }
+        else if (sum < minsum)
+        {
+            minsum = sum;
+            index = i;
+        }
+    }
+    string line = string.Empty;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        line += array[index, j] + " ";
+    }
+    Console.WriteLine($"Строка с минимальной суммой элементов >  {line}. ");
+}
+
+
+
+int m = Prompt("Введите размер прямоугольного массива: ");
+int[,] mas = CreateArray(m, m);
+System.Console.WriteLine();
+PrintArray(mas);
+System.Console.WriteLine();
+SumArrows(mas);
